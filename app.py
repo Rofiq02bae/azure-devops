6101feac71bf35d50708af7bbdb3c8fcfbc6528c
As'adur Rofiq <145300940+Rofiq02bae@users.noreply.github.com>
2025-06-30 12:40:28 +0700

    Update app.py dari github

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello from Azure Flask!'
+    return 'Halo dari CI/CD GitHub + Azure 💙'
+
 
 if __name__ == '__main__':
     app.run()
